b3ad68b4d133cdcb4a3edd04f7d8c6ed7fadacb2
Matt White <matt@matthewjwhite.co.uk>
2025-07-12 12:08:24 +0100

    try rehype formatter

--- dev-matthewjwhite/astro.config.mjs
+++ dev-matthewjwhite/astro.config.mjs
@@ -2,6 +2,7 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import rehypePrettyCode from "rehype-pretty-code";
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,9 +10,11 @@ export default defineConfig({
 	integrations: [mdx(), sitemap()],
 	markdown: {
 		//syntaxHighlight: 'prism',
-		shikiConfig: {
-			wrap: true,
-			theme: 'github-dark-dimmed',
-		}
+		//shikiConfig: {
+		//	wrap: true,
+		//	theme: 'github-dark-dimmed',
+		//}
+		syntaxHighlight: false,
+    	rehypePlugins: [[rehypePrettyCode, { theme: "github-dark" }]],
 	},
 });
